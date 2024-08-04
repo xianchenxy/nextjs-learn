61dae1335d88f8f62d4c9527b79e18e42d3e8e18
xianchenxy <xianchenxy@gmail.com>
2024-08-04 23:15:57 +0800

    feat: 完成前 4 章学习

--- app/ui/invoices/breadcrumbs.tsx
+++ app/ui/invoices/breadcrumbs.tsx
@@ -9,27 +9,27 @@ interface Breadcrumb {
 }
 
 export default function Breadcrumbs({
-  breadcrumbs,
-}: {
+                                      breadcrumbs
+                                    }: {
   breadcrumbs: Breadcrumb[];
 }) {
   return (
     <nav aria-label="Breadcrumb" className="mb-6 block">
-      <ol className={clsx(lusitana.className, 'flex text-xl md:text-2xl')}>
-        {breadcrumbs.map((breadcrumb, index) => (
+      <ol className={ clsx(lusitana.className, 'flex text-xl md:text-2xl') }>
+        { breadcrumbs.map((breadcrumb, index) => (
           <li
-            key={breadcrumb.href}
-            aria-current={breadcrumb.active}
-            className={clsx(
-              breadcrumb.active ? 'text-gray-900' : 'text-gray-500',
-            )}
+            key={ breadcrumb.href }
+            aria-current={ breadcrumb.active }
+            className={ clsx(
+              breadcrumb.active ? 'text-gray-900' : 'text-gray-500'
+            ) }
           >
-            <Link href={breadcrumb.href}>{breadcrumb.label}</Link>
-            {index < breadcrumbs.length - 1 ? (
+            <Link href={ breadcrumb.href }>{ breadcrumb.label }</Link>
+            { index < breadcrumbs.length - 1 ? (
               <span className="mx-3 inline-block">/</span>
-            ) : null}
+            ) : null }
           </li>
-        ))}
+        )) }
       </ol>
     </nav>
   );
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,26 +1,21 @@
 'use client';
 
 import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
-import {
-  CheckIcon,
-  ClockIcon,
-  CurrencyDollarIcon,
-  UserCircleIcon,
-} from '@heroicons/react/24/outline';
+import { CheckIcon, ClockIcon, CurrencyDollarIcon, UserCircleIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
 
 export default function EditInvoiceForm({
-  invoice,
-  customers,
-}: {
+                                          invoice,
+                                          customers
+                                        }: {
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
   return (
     <form>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
-        {/* Customer Name */}
+        {/* Customer Name */ }
         <div className="mb-4">
           <label htmlFor="customer" className="mb-2 block text-sm font-medium">
             Choose customer
@@ -30,22 +25,23 @@ export default function EditInvoiceForm({
               id="customer"
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-              defaultValue={invoice.customer_id}
+              defaultValue={ invoice.customer_id }
             >
               <option value="" disabled>
                 Select a customer
               </option>
-              {customers.map((customer) => (
-                <option key={customer.id} value={customer.id}>
-                  {customer.name}
+              { customers.map((customer) => (
+                <option key={ customer.id } value={ customer.id }>
+                  { customer.name }
                 </option>
-              ))}
+              )) }
             </select>
-            <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+            <UserCircleIcon
+              className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
           </div>
         </div>
 
-        {/* Invoice Amount */}
+        {/* Invoice Amount */ }
         <div className="mb-4">
           <label htmlFor="amount" className="mb-2 block text-sm font-medium">
             Choose an amount
@@ -57,16 +53,17 @@ export default function EditInvoiceForm({
                 name="amount"
                 type="number"
                 step="0.01"
-                defaultValue={invoice.amount}
+                defaultValue={ invoice.amount }
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               />
-              <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              <CurrencyDollarIcon
+                className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
         </div>
 
-        {/* Invoice Status */}
+        {/* Invoice Status */ }
         <fieldset>
           <legend className="mb-2 block text-sm font-medium">
             Set the invoice status
@@ -79,7 +76,7 @@ export default function EditInvoiceForm({
                   name="status"
                   type="radio"
                   value="pending"
-                  defaultChecked={invoice.status === 'pending'}
+                  defaultChecked={ invoice.status === 'pending' }
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
                 />
                 <label
@@ -95,7 +92,7 @@ export default function EditInvoiceForm({
                   name="status"
                   type="radio"
                   value="paid"
-                  defaultChecked={invoice.status === 'paid'}
+                  defaultChecked={ invoice.status === 'paid' }
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
                 />
                 <label
